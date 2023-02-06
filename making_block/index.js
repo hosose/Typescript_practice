7b18d5fb06446b7db7f5b4d65c6be3ee05b97d71
김세호 <sayho@Kimsehoui-noteubug.local>
2023-02-06 16:01:50 +0900

    Upadate: practice things

---
+++ making_block/index.js
@@ -0,0 +1,4 @@
+import { exit } from './mypackage';
+const a = exit(1);
+console.log(a);
+localStorage;
---
+++ making_block/index.ts
@@ -0,0 +1,3 @@
+import { init, exit } from './mypackage';
+init({ debug: true, url: 'cdc' });
+exit(1);
